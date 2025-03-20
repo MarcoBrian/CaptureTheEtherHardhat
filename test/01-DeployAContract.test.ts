ca99e1b8e8ca48992b118de7b3a1da3d0f653896
MarcoBrian <14990874+MarcoBrian@users.noreply.github.com>
2025-03-21 01:10:37 +0800

    challenge 1,2,4

--- test/01-DeployAContract.test.ts
+++ test/01-DeployAContract.test.ts
@@ -6,17 +6,17 @@ import { ethers } from 'hardhat';
 describe('DeployAContract', () => {
   let deployer: SignerWithAddress;
   let attacker: SignerWithAddress;
-  let target: Contract;
+  let target:any = null;
 
-  before(async () => {
+  beforeEach(async () => {
     [attacker, deployer] = await ethers.getSigners();
+    // Deploy the contract here
+    const TargetContract = await ethers.getContractFactory("DeployChallenge"); // Replace with your contract name
+    target = await TargetContract.deploy(); // Add constructor arguments if needed
+    await target.deployed();
   });
 
   it('exploit', async () => {
-    /**
-     * YOUR CODE HERE
-     * */
-
     expect(await target.isComplete()).to.equal(true);
   });
 });
